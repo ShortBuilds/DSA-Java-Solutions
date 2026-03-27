18d009d6eb434470411613455bdf447272f14466
Rudraksha Awasthi <89909288+ShortBuilds@users.noreply.github.com>
2026-03-27 21:31:43 +0530

    Create solution.java

---
+++ easy/Palindrome-Number-9/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x<0) return false;
+        int copy = x;
+        int rev=0;
+        while(copy!=0){
+             rev = rev*10 + (copy%10);
+             copy/=10;
+        }
+        if (rev ==x)return true;
+        else return false;
+    }
+}
