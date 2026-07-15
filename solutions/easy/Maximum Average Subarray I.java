58bcd20779223b635e9f045271892a0759c345ce
Rudraksha Awasthi <89909288+ShortBuilds@users.noreply.github.com>
2026-07-15 13:05:21 +0530

    Add solution for Maximum Average Subarray I

---
+++ solutions/easy/Maximum Average Subarray I.java
@@ -0,0 +1,16 @@
+// Title: Maximum Average Subarray I
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/maximum-average-subarray-i/
+
+        for (int i = 0; i < k; i++) {
+            currentSum += nums[i];
+        }
+        
+        int maxSum = currentSum;
+        
+    public double findMaxAverage(int[] nums, int k) {
+        int currentSum = 0;
+        
+        // Step 1: Calculate the sum of the very first window of size k
+class Solution {
