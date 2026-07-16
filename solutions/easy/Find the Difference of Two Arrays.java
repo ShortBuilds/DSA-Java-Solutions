ab64951a38102b99b2eb7ad3baa17a25c814cdc4
Rudraksha Awasthi <89909288+ShortBuilds@users.noreply.github.com>
2026-07-16 20:40:57 +0530

    Add solution for Find the Difference of Two Arrays

---
+++ solutions/easy/Find the Difference of Two Arrays.java
@@ -0,0 +1,26 @@
+// Title: Find the Difference of Two Arrays
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/find-the-difference-of-two-arrays/
+
+        set2.add(n);
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+
+        for (int n : set1){
+            if (!set2.contains(n))
+        }
+            list1.add(n);
+
+        for (int n : set2){
+            set1.add(n);
+        for (int n : nums2){
+        }
+        }
+            if (!set1.contains(n))
+            list2.add(n);
+        }
+
+        return Arrays.asList(list1,list2);
+    }
+}
