0ff2f0563f50c9857f5dc8bfdc402d78a2034fd8
Rudraksha Awasthi <89909288+ShortBuilds@users.noreply.github.com>
2026-07-17 15:50:10 +0530

    Add solution for Unique Number of Occurrences

---
+++ solutions/easy/Unique Number of Occurrences.java
@@ -0,0 +1,21 @@
+// Title: Unique Number of Occurrences
+            // Difficulty: Easy
+            // Language: Java
+            // Link: https://leetcode.com/problems/unique-number-of-occurrences/
+
+        for (int i : arr){
+            ca[i + 1000]++;
+        }
+        Arrays.sort(ca);
+
+        for (int i =0; i<2000; i++){
+            if (ca[i] !=0 && ca[i]==ca[i+1])
+        }
+    }
+            return false;
+
+        return true;
+
+        int ca[] = new int[2001];
+    public boolean uniqueOccurrences(int[] arr) {
+class Solution {
