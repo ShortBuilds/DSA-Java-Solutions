f56ab5addb6f5e54b7cdbd3f9cd360ea394238b6
Rudraksha Awasthi <89909288+rudraksha-awasthi@users.noreply.github.com>
2026-07-22 11:29:20 +0530

    Time: 0 ms (100.00%) | Memory: 44.1 MB (84.23%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -14,21 +14,15 @@ class Solution {
         ListNode curr = head;
         
         while (curr != null) {
-            // 1. Save the next node so we don't lose the rest of the list
             ListNode nextTemp = curr.next;
             
-            // 2. Actually reverse the pointer to point backwards
             curr.next = prev;
             
-            // 3. Move the 'prev' pointer forward one step
             prev = curr;
             
-            // 4. Move the 'curr' pointer forward one step (using our saved reference)
             curr = nextTemp;
         }
         
-        // When the loop finishes, 'curr' is null (fell off the end).
-        // 'prev' is standing on the very last node, which is our new head.
         return prev;
     }
 }
