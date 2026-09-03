c40ce6fc08858f181d2f1a87630c7797c687bf61
Rudraksha Awasthi <89909288+rudraksha-awasthi@users.noreply.github.com>
2026-09-03 12:06:34 +0530

    Time: 7 ms (68.10%) | Memory: 44 MB (73.93%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.java
+++ 567-permutation-in-string/permutation-in-string.java
@@ -1,42 +1,24 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
 
-        if (s1.length() > s2.length()) {
-            return false;
-        }
+        if (s1.length()> s2.length()) return false;
 
-        int[] count1 = new int[26];
-        int[] count2 = new int[26];
-
-
-        for (char c : s1.toCharArray()) {
+        int count1[] = new int[26];
+        int count2[] = new int[26];
+        for (char c : s1.toCharArray()){
             count1[c - 'a']++;
         }
 
-        int windowSize = s1.length();
+        int k = s1.length();
 
+         for (int i =0 ;i<s2.length(); i++){
 
-        for (int i = 0; i < windowSize; i++) {
             count2[s2.charAt(i) - 'a']++;
-        }
 
-        if (Arrays.equals(count1, count2)) {
-            return true;
-        }
+            if (i>=k) count2[s2.charAt(i-k)-'a']--;
 
-
-        for (int right = windowSize; right < s2.length(); right++) {
-
-
-            count2[s2.charAt(right) - 'a']++;
-
-
-            int left = right - windowSize;
-            count2[s2.charAt(left) - 'a']--;
-
-            if (Arrays.equals(count1, count2)) {
+            if (Arrays.equals(count1, count2))
                 return true;
-            }
         }
 
         return false;
